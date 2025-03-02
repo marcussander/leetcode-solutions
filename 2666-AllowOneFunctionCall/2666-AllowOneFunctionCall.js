23a581b890e26bceee0f67e4ca3c45c2da06f6c7
marcussander <90462746+marcussander@users.noreply.github.com>
2025-03-02 15:28:39 -0500

    Create file

---
+++ 2666-AllowOneFunctionCall/2666-AllowOneFunctionCall.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let alreadyCalled = false;
+    return function(...args){
+        if(!alreadyCalled){
+            alreadyCalled = true;
+            return fn(...args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
